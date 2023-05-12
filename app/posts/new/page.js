62947926991e506ab527f3ea0cbed47882683ce8
rinZee <trex33160@gmail.com>
2023-05-12 23:19:24 +0545

    created a new route for posts and nested another route in it called new

---
+++ app/posts/new/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// the route is http://localhost:3000/posts/new
+
+const page = () => {
+  return (
+    <div>
+      <p>this is a post 1</p>
+    </div>
+  );
+};
+
+export default page;
