3a14f682f156c4e825919da8e657d87d72d81a59
rinZee <trex33160@gmail.com>
2023-05-16 23:17:44 +0545

    added some text describing about Alike

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,20 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Find and Meet
+        <br />
+        <span className="orange_gradient text-center">
+          People who share your Taste in movies
+        </span>
+      </h1>
+
+      <p className="desc text-center">
+        ShowShare is an world to discover and find people have similar taste in
+        movies
+      </p>
+    </section>
+  );
 };
 
 export default Home;
